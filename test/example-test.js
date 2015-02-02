bfb09484c3984e350fbfb7d8fef36ebb919a50b4
Gregor Martynus <gregor@martynus.net>
2015-02-02 16:51:59 +0100

    test: require external library

--- test/example-test.js
+++ test/example-test.js
@@ -1,9 +1,10 @@
 /* globals describe, it, expect */
 
+var foo = require('./example-lib');
+
 describe('myLib', function () {
   it('should pass', function () {
-    var foo = 'bar';
-    expect(foo).to.equal('bar');
+    expect(foo()).to.equal('bar');
   });
   it.skip('should not pass', function () {
     expect('foo').to.equal('bar');
